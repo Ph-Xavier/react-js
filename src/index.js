b7a51864f8bb23addd88d73cfcd0a42ee33af347
Pedro Xavier <pedroxxavier@hotmail.com>
2024-05-09 15:07:05 -0300

    Componentes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import Button from "./components/Button";
 
 class App extends React.Component {
   render() {
-    return <h1>Hello React!!</h1>;
+    return (
+      <h1>
+        Hello React!!
+        <Button />
+      </h1>
+    );
   }
 }
 
